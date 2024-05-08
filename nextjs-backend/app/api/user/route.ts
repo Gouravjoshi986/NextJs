4b8afa21ed758c930b81be9b7bfb9ed9ce1788ab
Gouravjoshi986 <gourav.joshi2003@gmail.com>
2024-05-08 19:41:05 +0530

    updated sign up route frontend and backend

--- nextjs-backend/app/api/user/route.ts
+++ nextjs-backend/app/api/user/route.ts
@@ -1,3 +1,13 @@
-export async function GET() {
-    return Response.json({ username: "harkirat", email: "harkirat@gmail.com" })
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+    return NextResponse.json({ username: body.username, password: body.password })
   }
+
+  export async function GET() {
+      return NextResponse.json({
+        username: "harkirat",
+        email: "harkirat@gmail.com" 
+      })
+    }
